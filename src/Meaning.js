2005bae286a7c37f92fffea5b6344f7e5faff800
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-01 17:36:23 -0400

    Added Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,5 +2,17 @@ import React from "react";
 
 export default function Meaning(props) {
     console.log(props.meaning);
-    return "Hello from Meaning";
+    return (
+        <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function(definition, index) {
+            return (
+            <div key={index}>
+                <p>{definition.definition}</p>
+                <p>{definition.example}</p>
+                </div>
+                );
+            })}
+        </div>
+    );
 }

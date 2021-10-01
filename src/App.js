c330c62d828cb7465c7bf259827b80976e0a84e9
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-01 11:39:27 -0400

    Added Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.jpg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a href="#" className="btn btn-primary shadow">Hello</a>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Annie</footer>
+      </div>
     </div>
   );
 }

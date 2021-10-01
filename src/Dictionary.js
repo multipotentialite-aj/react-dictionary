f00cc8c5ebb58c68f0f26352378177488d3a0848
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-01 15:20:18 -0400

    Added Results.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,9 +5,11 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
         console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export default function Results() {
-    return (
-        <div className="Results">
-            Hello from Results
-            </div>
-    )
+export default function Results(props) {
+    console.log(props.results);
+    if (props.results) {
+        return <div className="Results">
+            <h2>{props.results.word}</h2>
+            {props.results.meanings.map(function(meaning, index) {return meaning.definitions[0]
+            })}
+        </div>;  
+     } else {
+        return null;
+     }
 }

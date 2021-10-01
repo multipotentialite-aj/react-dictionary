ca33aa82a71a7c1fccf6a57e45f1256c8e9a409c
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-01 11:14:49 -0400

    Added button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <a href="#" className="btn btn-primary shadow">Hello</a>
       </header>
     </div>
   );

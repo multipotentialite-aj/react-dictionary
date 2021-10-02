a5a89d56221f7ed1f239e9bba01f43f88b8cc7eb
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-02 19:19:57 -0400

    Fixed App.js
    Added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
         <Dictionary defaultKeyword="synesthesia" />
       </main>
       <footer className="App-footer"><small>Coded by Annie Jong •
-      <a href="mailto:annie@bluesy-owl.design" target="_blank" className="m-1"><i class="fas fa-at"></i></a> 
-      <a href="https://www.linkedin.com/in/aj1016/" target="_blank" className="m-1"><i class="fab fa-linkedin"></i></a> 
-      <a href="https://github.com/multipotentialite-aj/react-dictionary" target="_blank" className="m-1"><i class="fab fa-github-square"></i></a></small></footer>
+      <a href="mailto:annie@bluesy-owl.design" target="_blank" className="m-1" rel="noreferrer"><i class="fas fa-at"></i></a> 
+      <a href="https://www.linkedin.com/in/aj1016/" target="_blank" className="m-1" rel="noreferrer"><i class="fab fa-linkedin"></i></a> 
+      <a href="https://github.com/multipotentialite-aj/react-dictionary" target="_blank" className="m-1" rel="noreferrer"><i class="fab fa-github-square"></i></a></small></footer>
       </div>
     </div>
   );

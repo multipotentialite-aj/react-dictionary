247e2e90617c33d3b26fe593a11061ff5dc948a5
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-02 19:22:16 -0400

    Fixed Phonetic.js
    Added rel="noreferrer"

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
     console.log(props.phonetic);
     return (
         <div class="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
             <i class="fa-solid fa-circle-play">Listen</i>
             </a>
             <span className="text">{props.phonetic.text}</span>

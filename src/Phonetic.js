23c2fe102b06b7b3ca741b7545c6341cf0c215bd
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-01 20:52:23 -0400

    Added Phonetic.js

---
+++ src/Phonetic.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Phonetic(props) {
+    console.log(props.phonetic);
+    return (
+        <div class="Phonetic">
+            <a href={props.phonetic.audio} target="_blank">
+            <i class="fa-solid fa-circle-play">Listen</i>
+            </a>
+            <br />
+            {props.phonetic.text}
+            </div>
+    );
+}

60d9a871c66c0b10d56b21bd23573d6c254b1506
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-01 20:22:52 -0400

    Added Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,13 @@ import React from "react";
 
 export default function Synonyms(props) {
     if (props.synonyms) {
-    return "Hello from Synonyms";
+    return (
+        <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+            return <li key={index}>{synonym}</li>;
+        })}
+        </ul>
+    );
     } else {
         return null
     }

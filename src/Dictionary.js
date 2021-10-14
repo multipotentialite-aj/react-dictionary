8f35736dc5c784e84f28e6e8ffce6eb52e0e7cc8
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-10-14 15:06:11 -0400

    Removed button

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -41,8 +41,6 @@ export default function Dictionary() {
         <form onSubmit={search}>
             <input type="search" placeholder="Synaesthesia"
             onChange={handleKeywordChange} />
-            <span className="btn btn-light ms-3"
-            onClick={handleKeywordChange}>Search</span>
             </form>
             <div className="Hint">
                 Suggested words: desolate, void, lugubrious, bleak...
